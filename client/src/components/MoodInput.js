dc0047e264c90bba4edcc7e74f06c294d0020e33
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2025-05-31 23:46:29 +0530

    changed index.html name

--- client/src/components/MoodInput.js
+++ client/src/components/MoodInput.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useDropzone } from 'react-dropzone';
+// import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
 
 const MoodInput = () => {
